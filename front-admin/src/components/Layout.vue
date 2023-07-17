6e0bce48c7a92ed9550b1c15867e45ce721a2f7d
RSelwa <selwa.raphael@gmail.com>
2023-07-17 09:55:27 +0200

    🔥 clean code base

--- front-admin/src/components/Layout.vue
+++ front-admin/src/components/Layout.vue
@@ -1,11 +1,16 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import IconCommunity from './icons/IconCommunity.vue'
+import VueLink from '@/src/components/ui/VueLink'
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
+  <header class="bg-green-100 flex items-center justify-between px-4">
+    <div class="h-8 w-8 bg-red-400 flex items-center justify-center">
+      <IconCommunity />
+    </div>
+    <nav class="flex gap-4">
+      <VueLink to="/">sdfsdfsdf</VueLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/test">Test</RouterLink>
     </nav>
---
+++ front-admin/src/components/ui/VueLink.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps(['foo'])
+
+console.log(props.foo)
+</script>
+
+<template>
+      <RouterLink to="/">Hsdfsdfsdfome</RouterLink>
+</template>
