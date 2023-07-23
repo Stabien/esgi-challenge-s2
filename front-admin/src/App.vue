f0834fed1d3decaf78d25efe7647c4e583c28cd4
RSelwa <selwa.raphael@gmail.com>
2023-07-23 12:48:17 +0200

    💄 Fix: Stylize front-admin

--- front-admin/src/App.vue
+++ front-admin/src/App.vue
@@ -1,42 +1,36 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import Layout from '@/components/Layout.vue'
-import { getLogo } from '@/utils'
-import { ref } from 'vue'
+import { RouterView } from 'vue-router';
+import Layout from '@/components/Layout.vue';
+import { getLogo } from '@/utils';
+import { provide, ref } from 'vue';
+
 // Change FavIcon
-var link = document.querySelector("link[rel~='icon']")
+var link = document.querySelector("link[rel~='icon']");
 if (!link) {
-  link = document.createElement('link')
-  link.rel = 'icon'
-  document.head.appendChild(link)
+  link = document.createElement('link');
+  link.rel = 'icon';
+  document.head.appendChild(link);
 }
-link.href = getLogo()
+link.href = getLogo();
 
-//Custom cursor
-const mouseX = ref(0)
-const visibility = ref(0)
-const mouseY = ref(0)
+const user = ref({ isLogged: true });
 
-window.addEventListener('mousemove', (e) => {
-  if (e.clientX < window.innerWidth - 16 / 2) mouseX.value = e.clientX
-  if (e.clientY < window.innerHeight - 16 / 2) mouseY.value = e.clientY
-})
-document.addEventListener('mouseenter', () => {
-  console.log('je suis dedans')
-  visibility.value = 'visible'
-})
-document.addEventListener('mouseout', () => {
-  console.log('je suis dehors')
-  // visibility.value = 'hidden'
-})
+const setIsLogged = (isLogged) => {
+  user.value.isLogged = isLogged;
+};
+
+provide('user', {
+  user,
+  setIsLogged
+});
 </script>
 
 <template>
   <Layout v-if="$route.meta.layout !== 'none'" />
   <RouterView class="pt-24" />
-  <span
+  <!-- <span
     ref="cursor"
     :style="{ left: mouseX + 'px', top: mouseY + 'px', visibility: visibility }"
     class="fixed origin-center h-4 w-4 bg-palette-primary-500 mix-blend-difference invert rounded-full opacity-60 pointer-events-none"
-  ></span>
+  ></span> -->
 </template>
--- vitrine/src/components/ui/Button.vue
+++ vitrine/src/components/ui/Button.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { cva } from 'class-variance-authority'
-import { cn } from '@/utils'
+import { cva } from 'class-variance-authority';
+import { cn } from '@/utils';
 
-defineProps({ variant: { default: 'default' } })
+defineProps({ variant: { default: 'default' } });
 
-const size = 'default'
+const size = 'default';
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md font-medium transition-colors duration-150 disabled:pointer-events-none focus:outline-none disabled:opacity-50 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800',
   // Focus ring if necessary -> "focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900"
@@ -12,7 +12,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-palette-primary-500 text-white hover:bg-palette-primary-600 dark:bg-palette-primary-400 dark:hover:bg-palette-primary-600 dark:text-white',
+          'bg-palette-primary-500 text-white hover:bg-palette-primary-600 dark:bg-palette-primary-400  border  border-palette-primary-500  dark:hover:bg-palette-primary-600 dark:text-white',
         destructive: 'bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600',
         outline:
           'bg-transparent text-palette-primary-500 border border-palette-primary-500 hover:bg-palette-primary-50 dark:border-palette-primary-400 dark:text-palette-primary-400 dark:hover:bg-palette-primary-900 dark:hover:text-palette-primary-400',
@@ -34,7 +34,7 @@ const buttonVariants = cva(
       size: 'default'
     }
   }
-)
+);
 </script>
 
 <template>
