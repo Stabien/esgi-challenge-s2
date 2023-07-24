df9024cd7bd5aebe57db8e89b869bd3acfceb725
RSelwa <selwa.raphael@gmail.com>
2023-07-24 16:36:46 +0200

    🎨 Feat: Register form

---
+++ front-admin/src/components/ui/Input.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps(['type', 'label', 'modelValue', 'required']);
+defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <label :for="props.label"
+    >{{ props.label }}
+    <span v-if="props.required" class="text-palette-primary-500">*</span>
+  </label>
+
+  <input
+    v-if="props.type !== 'file'"
+    :required="props.required"
+    :type="props.type"
+    :id="props.label"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+  <input
+    v-else
+    :required="props.required"
+    accept="application/pdf"
+    :type="props.type"
+    :id="props.label"
+    @change="(event) => console.log(event)"
+  />
+</template>
