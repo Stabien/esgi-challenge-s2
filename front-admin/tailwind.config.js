d9b9517cdca9245aff369c17461d712f9364a7a4
RSelwa <selwa.raphael@gmail.com>
2023-07-27 16:27:09 +0200

    💄 Home for Bastien, c'est bon ta page d'acceuil

--- front-admin/tailwind.config.js
+++ front-admin/tailwind.config.js
@@ -54,7 +54,7 @@ module.exports = {
           '100%': { opacity: 1, transform: 'translateY(0)' }
         },
         slideRightAndFade: {
-          '0%': { opacity: 0, transform: 'translateX(-2px)' },
+          '0%': { opacity: 0, transform: 'translateX(-2000px)' },
           '100%': { opacity: 1, transform: 'translateX(0)' }
         },
         slideDownAndFade: {
@@ -62,7 +62,7 @@ module.exports = {
           '100%': { opacity: 1, transform: 'translateY(0)' }
         },
         slideLeftAndFade: {
-          '0%': { opacity: 0, transform: 'translateX(2px)' },
+          '0%': { opacity: 0, transform: 'translateX(2000px)' },
           '100%': { opacity: 1, transform: 'translateX(0)' }
         }
       },
@@ -85,4 +85,4 @@ module.exports = {
     extend: {}
   },
   plugins: []
-}
+};
