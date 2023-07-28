ae94305e265119776cb4a62ed37b956ea6a14202
Mevin Redureau <mel.redureau@gmail.com>
2023-07-28 17:44:16 +0200

    :art: Modify Tracker.vue inside vitrine and delete unused code inside the RegisterWithPassword.vue inside vitrine

--- vitrine/src/utils/Tracker.vue
+++ vitrine/src/utils/Tracker.vue
@@ -1,112 +1,107 @@
 <script>
-const exportData = async ({
-  appId,
-  event,
-  url,
-  sessionId,
-  htmlElement,
-  directiveTag,
-  timestamp
-}) => {
-  try {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    var requestOptions = {
-      method: 'POST',
-      body: JSON.stringify({
-        appId,
-        event,
-        url,
-        sessionId,
-        htmlElement,
-        directiveTag,
-        timestamp
-      }),
-      headers
-    };
-    const response = await fetch(
-      `${import.meta.env.VITE_PROD_API_URL}/api/analytics/add`,
-      requestOptions
-    );
-    if (!response.ok) throw new Error('Something went wrong');
-    // const data = await response.json();
-    // updateLocalStorage('token', props.isAdmin ? data.admin.token : data.user.token);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-function handleURL() {
-  checkUrls();
-}
-
-function checkUrls() {
-  if (window.localStorage.getItem('url') !== getURL()) window.localStorage.setItem('url', getURL());
-}
-
-function getURL() {
-  return window.location.pathname;
-}
-
-function setSessionID(APP_ID) {
-  /* Trigger aux changement des conditions */
-  let SessionId = window.localStorage.getItem('Session_ID');
-  exportData({
-    appId: APP_ID,
-    event: 'newSession',
-    url: getURL(),
-    sessionId: SessionId,
-    htmlElement: 'none',
-    directiveTag: 'none',
-    timestamp: Date.now()
-  });
-  return SessionId;
-}
-
-function handleEvent(element, eventName, directiveBindingArgument, APP_ID) {
-  const htmlElement = element.tagName;
-  // const tagId = element.id;
-  handleURL();
-  exportData({
-    appId: APP_ID,
-    event: eventName,
-    url: getURL(),
-    sessionId: window.localStorage.getItem('Session_ID'),
-    htmlElement: htmlElement,
-    directiveTag: directiveBindingArgument,
-    timestamp: Date.now()
-  });
-}
-// const mouseX = ref(0);
-// const mouseY = ref(0);
-// function handleMouse(element, e, bArg) {
-//   if (e.clientX < window.innerWidth - 16 / 2) mouseX.value = e.clientX;
-//   if (e.clientY < window.innerHeight - 16 / 2) mouseY.value = e.clientY;
-// }
-
-window.localStorage.setItem('url', getURL());
-// if (!window.localStorage.getItem('Session_ID')) {
-//   const token = jwt_decode(localStorage.getItem('token'));
-//   if (token) {
-//     console.log(token);
-//     window.localStorage.setItem('Session_ID', setSessionID(token.token.appId));
-//   }
-// }
-
-window.addEventListener('unload', () => {
-  // window.localStorage.removeItem('Session_ID');
-});
-// window.localStorage.setItem('url', getURL());
-// window.localStorage.setItem('Session_ID', setSessionID(APP_ID));
-
-const itemExistsInLocalStorage = (key) => {
-  const value = localStorage.getItem(key);
-  return value !== null;
-};
-
 export default {
   install(VueInstance, options) {
     let APP_ID = options.App_id;
+
+    const exportData = async ({
+                                appId,
+                                event,
+                                url,
+                                sessionId,
+                                htmlElement,
+                                directiveTag,
+                                timestamp
+                              }) => {
+      try {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let requestOptions = {
+          method: 'POST',
+          body: JSON.stringify({
+            appId,
+            event,
+            url,
+            sessionId,
+            htmlElement,
+            directiveTag,
+            timestamp
+          }),
+          headers
+        };
+        const response = await fetch(
+            `${import.meta.env.VITE_PROD_API_URL}/api/analytics/add`,
+            requestOptions
+        );
+        if (!response.ok) throw new Error('Something went wrong');
+        // const data = await response.json();
+        // updateLocalStorage('token', props.isAdmin ? data.admin.token : data.user.token);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    function handleURL(APP_ID) {
+      setTimeout(() => {
+        if (window.localStorage.getItem('url') !== getURL()) {
+          window.localStorage.setItem('url', getURL());
+          console.log(window.localStorage.getItem('url'))
+          exportData({
+            appId: APP_ID,
+            event: 'navigation',
+            url: getURL(),
+            sessionId: window.localStorage.getItem('Session_ID'),
+            htmlElement: 'none',
+            directiveTag: 'none',
+            timestamp: Date.now()
+          });
+        }
+      },100)
+    }
+
+    function getURL() {
+      return window.location.pathname;
+    }
+
+    function setSessionID(APP_ID) {
+      /* Trigger aux changement des conditions */
+      let SessionId = window.localStorage.getItem('Session_ID');
+      console.log(SessionId)
+      exportData({
+        appId: APP_ID,
+        event: 'newSession',
+        url: getURL(),
+        sessionId: SessionId,
+        htmlElement: 'none',
+        directiveTag: 'none',
+        timestamp: Date.now()
+      });
+      return SessionId;
+    }
+
+    function handleEvent(element, eventName, directiveBindingArgument, APP_ID) {
+      const htmlElement = element.tagName;
+      exportData({
+        appId: APP_ID,
+        event: eventName,
+        url: getURL(),
+        sessionId: window.localStorage.getItem('Session_ID'),
+        htmlElement: htmlElement,
+        directiveTag: directiveBindingArgument,
+        timestamp: Date.now()
+      });
+    }
+
+    window.localStorage.setItem('url', getURL());
+
+    window.addEventListener('close', () => {
+      window.localStorage.removeItem('Session_ID');
+    });
+
+    const itemExistsInLocalStorage = (key) => {
+      const value = localStorage.getItem(key);
+      return value !== null;
+    };
+
     if (!itemExistsInLocalStorage('Session_ID')) {
       window.localStorage.setItem(
         'Session_ID',
@@ -114,7 +109,10 @@ export default {
       );
       setSessionID(APP_ID);
     }
-    VueInstance.provide('app_id', APP_ID);
+
+    setInterval(() => {
+      handleURL(APP_ID)
+    },500)
 
     VueInstance.directive('track', {
       mounted: (el, binding) => {
@@ -125,10 +123,10 @@ export default {
         });
       }
     });
-
     VueInstance.directive('mouse', {
       mounted: () => {}
     });
-  }
+  },
 };
 </script>
+
