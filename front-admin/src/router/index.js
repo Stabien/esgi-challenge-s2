a793d67ddd6bb08631506e35b4a080e78fc397a9
RSelwa <selwa.raphael@gmail.com>
2023-08-02 14:23:25 +0200

    🚧 Work on account settings

--- front-admin/src/router/index.js
+++ front-admin/src/router/index.js
@@ -41,7 +41,7 @@ const router = createRouter({
     {
       path: '/account-settings',
       name: 'account-settings',
-      component: () => import('@/views/AccountSettingsView.vue')
+      component: () => import('@/views/RequestView.vue')
     },
     {
       path: '/admin-user/:uid',
--- front-admin/src/views/RequestView.vue
+++ front-admin/src/views/RequestView.vue
@@ -1,13 +1,48 @@
 <script setup>
-import { useRoute } from 'vue-router';
-import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { inject, onMounted, ref } from 'vue';
 import Link from '@/components/ui/Link.vue';
 import { REJECTED, VALIDATED, PENDING } from '@/utils/requestConstants';
 
 const route = useRoute();
-const requestUid = route.params.uid;
+const router = useRouter();
+const { user } = inject('user');
+const requestUid = route.params.uid || user.value.decodedToken.uuid;
+
 const userRequest = ref();
 const isLoading = ref(false);
+const isEditing = ref(false);
+
+const toggleEdit = () => (isEditing.value = !isEditing.value);
+const cancelEdit = () => {
+  console.log('cancel');
+  isEditing.value = !isEditing.value;
+  fetchUserRequest();
+};
+const saveEdit = async () => {
+  try {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    await fetch(
+      `${import.meta.env.VITE_PROD_API_URL}/api/user/${requestUid}`,
+      JSON.stringify(userRequest.value),
+      {
+        method: 'PUT',
+        body: JSON.stringify(userRequest.value),
+        headers: {
+          headers,
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      }
+    );
+    console.log(userRequest.value);
+  } catch (error) {
+    console.log(error);
+  }
+  isEditing.value = !isEditing.value;
+};
+
+const isMe = () => requestUid === user.value.decodedToken.uuid;
 
 const getCorrectRequest = (status) => {
   switch (status) {
@@ -61,7 +96,6 @@ const fetchUserRequest = async () => {
 
     const data = await response.json();
     userRequest.value = data;
-    console.log(data);
   } catch (error) {
     console.log(error);
   }
@@ -70,6 +104,8 @@ const fetchUserRequest = async () => {
 
 onMounted(() => {
   fetchUserRequest();
+  console.log(user);
+  if (!user.value.isLogged) router.push('/');
 });
 </script>
 
@@ -83,7 +119,7 @@ onMounted(() => {
       class="grid grid-cols-2 border border-palette-gray-300 gap-4 p-4 rounded mx-40"
     >
       <div class="flex gap-2 items-center">
-        <Link to="/admin" variant="ghost" class=""
+        <Link :to="user.isAdmin ? '/admin' : '/'" variant="ghost" class=""
           ><ArrowLeft class="h-6 w-6 text-palette-primary-500"
         /></Link>
         <div class="text-palette-primary-500 text-4xl uppercase font-bold">
@@ -114,18 +150,28 @@ onMounted(() => {
         }"
         class="grid grid-cols-2 gap-4"
       >
-        <Input disabled type="text" label="email" v-model="userRequest.email" />
-        <Input disabled type="text" label="appId" v-model="userRequest.appId" />
-        <Input disabled type="text" label="firstname" v-model="userRequest.firstname" />
-        <Input disabled type="text" label="lastname" v-model="userRequest.lastname" />
-        <Input disabled type="text" label="society" v-model="userRequest.societyName" />
+        <Input :disabled="!isEditing" type="text" label="email" v-model="userRequest.email" />
+        <Input :disabled="!isEditing" type="text" label="appId" v-model="userRequest.appId" />
+        <Input
+          :disabled="!isEditing"
+          type="text"
+          label="firstname"
+          v-model="userRequest.firstname"
+        />
+        <Input :disabled="!isEditing" type="text" label="lastname" v-model="userRequest.lastname" />
+        <Input
+          :disabled="!isEditing"
+          type="text"
+          label="society"
+          v-model="userRequest.societyName"
+        />
         <div
           :style="{
             'grid-area': 'url'
           }"
           class="flex gap-2 items-end"
         >
-          <Input disabled type="text" label="url" v-model="userRequest.url" />
+          <Input :disabled="!isEditing" type="text" label="url" v-model="userRequest.url" />
           <a target="_blank" :href="userRequest.url"
             ><Button variant="outline"
               ><ExternalLink class="h-6 w-6 text-palette-primary-500" /></Button
@@ -135,26 +181,36 @@ onMounted(() => {
           :style="{
             'grid-area': 'buttons'
           }"
-          class="flex gap-2 items-end place-self-end"
         >
-          <Button
-            v-if="userRequest.status === PENDING"
-            variant="outline"
-            @click="() => handleRequest(VALIDATED, userRequest.uuid)"
-          >
-            <CheckIcon height="24" width="24" class="text-palette-green mr-2" />Validate
-          </Button>
-          <Button
-            v-if="userRequest.status === PENDING"
-            variant="outline"
-            @click="() => handleRequest(REJECTED, userRequest.uuid)"
-          >
-            <RefusedIcon height="24" width="24" class="text-palette-primary-500 mr-2" />Reject
-          </Button>
-          <Button v-else variant="outline" @click="() => handleRequest(PENDING, userRequest.uuid)">
-            <PendingIcon height="24" width="24" class="text-palette-gray-300 mr-2" />
-            Go back to pending
-          </Button>
+          <div class="flex gap-2 items-end justify-end" v-if="user.status === 'Admin'">
+            <Button
+              v-if="userRequest.status === PENDING"
+              variant="outline"
+              @click="() => handleRequest(VALIDATED, userRequest.uuid)"
+            >
+              <CheckIcon height="24" width="24" class="text-palette-green mr-2" />Validate
+            </Button>
+            <Button
+              v-if="userRequest.status === PENDING"
+              variant="outline"
+              @click="() => handleRequest(REJECTED, userRequest.uuid)"
+            >
+              <RefusedIcon height="24" width="24" class="text-palette-primary-500 mr-2" />Reject
+            </Button>
+            <Button
+              v-else
+              variant="outline"
+              @click="() => handleRequest(PENDING, userRequest.uuid)"
+            >
+              <PendingIcon height="24" width="24" class="text-palette-gray-300 mr-2" />
+              Go back to pending
+            </Button>
+          </div>
+          <div class="flex gap-2 items-end justify-end" v-else-if="isMe()">
+            <Button v-if="!isEditing" variant="outline" @click="toggleEdit">Edit</Button>
+            <Button v-if="isEditing" variant="outline" @click="cancelEdit">Cancel</Button>
+            <Button v-if="isEditing" variant="default" @click="saveEdit">Save</Button>
+          </div>
         </div>
       </div>
     </div>
