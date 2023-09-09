7f8fc4a1e9a7237be94d62a4f91ea366905ff6ad
BASTIEN PIEDALLU <bastien.piedallu@gmail.com>
2023-09-09 17:06:32 +0200

    revert merge

--- back/src/controllers/analyticsController.js
+++ back/src/controllers/analyticsController.js
@@ -1,6 +1,7 @@
 const Analytics = require('../models/analytics')
 const Tags = require('../models/tags')
 const Graphs = require('../models/graphs')
+const Alerts = require('../models/alerts')
 const { getIsoDateFromTimestamp } = require('../helpers')
 
 exports.postGraphSettings = async (req, res) => {
@@ -55,6 +56,12 @@ exports.deleteGraphSettings = async (req, res) => {
         uuid: uuid,
       },
     })
+    Alerts.destroy({
+      where: {
+        graphUuid: uuid,
+      },
+    })
+
     return res.status(201).json(tag)
   } catch (error) {
     console.log(error)
@@ -63,11 +70,11 @@ exports.deleteGraphSettings = async (req, res) => {
 }
 
 exports.addAnalytics = async (req, res) => {
-  const { body } = req
-  const analytics = new Analytics(body)
-
   try {
+    const { body } = req
+    const analytics = new Analytics(body)
     await analytics.save()
+    handleAlerts(body)
     return res.status(201).json(analytics)
   } catch (e) {
     console.log(e)
@@ -202,5 +209,144 @@ exports.getHeatmapData = async (req, res) => {
   } catch (e) {
     console.log(e)
     return res.status(500).json({ error: 'Internal error' })
+  }
+}
+
+const handleAlerts = async (body) => {
+  try {
+    //1-Fetch alerts by appId
+    const alertsList = await Alerts.findAll({ where: { appId: body.appId } })
+
+    //2-Fetch graphs that are associated to the alerts
+    let associedGraph = new Array(alertsList.length).fill(undefined)
+    for (const [index, alertsDoc] of alertsList.entries()) {
+      const graphUuid = alertsDoc.dataValues.graphUuid
+      const graph = await Graphs.findOne({ where: { uuid: graphUuid } })
+      associedGraph[index] = graph.dataValues
+    }
+    if (associedGraph.length <= 0) return
+
+    //3-For each associed Graph, get the data
+    let associedData = new Array(associedGraph.length).fill(undefined)
+    for (const [index, graphSettings] of associedGraph.entries()) {
+      const { timeScale, tagUuid, event } = graphSettings
+      const { appId } = body
+      const { start, end } = getIsoDateFromTimestamp(timeScale)
+      const aggregateTunnel = [
+        { $match: { appId } },
+        {
+          $match: {
+            timestamp: {
+              $gte: new Date(start),
+              $lt: new Date(end),
+            },
+          },
+        },
+      ]
+      if (event === 'CTR') {
+        aggregateTunnel.push({
+          $match: {
+            $or: [{ event: 'click' }, { event: 'print' }],
+          },
+        })
+      } else if (event === 'funnel') {
+      } else {
+        aggregateTunnel.push({ $match: { event } })
+      }
+      if (tagUuid.length > 0) {
+        const tagList = []
+        for (const tagUuidItem of tagUuid) {
+          const tags = await Tags.findOne({ where: { uuid: tagUuidItem } })
+          tagList.push(tags.dataValues)
+        }
+        aggregateTunnel.push({
+          $match: {
+            $or: tagList.map((t) => ({
+              directiveTag: t.name,
+            })),
+          },
+        })
+
+        // aggregateTunnel.push({ $match: { directiveTag: tags.dataValues.name } })
+      }
+      const analytics = await Analytics.aggregate(aggregateTunnel)
+      associedData[index] = analytics
+    }
+
+    //4-check if need to send alerts
+    for (const [index, graph] of associedGraph.entries()) {
+      let value = null
+      console.log(graph.event)
+      switch (graph.event) {
+        case 'click':
+          console.log('click')
+          value = getLength(graph, associedData[index])
+
+        case 'newSession':
+          console.log('newMission')
+          value = getLength(graph, associedData[index])
+
+        case 'print':
+          console.log('print')
+          value = getLength(graph, associedData[index])
+        case 'CTR':
+          console.log('CTR')
+          value = getCTRBy(graph, associedData[index])
+        case 'funnel':
+          console.log('funnel')
+          value = await getFunnel(graph, associedData[index])
+
+        default:
+          value = getLength(graph, associedData[index])
+      }
+      console.log('value', value)
+
+      const valueToTrigger = parseInt(alertsList[index].dataValues.valueToTrigger)
+      if (value >= valueToTrigger) {
+        console.log('GONNA SEND ALERTS')
+      }
+    }
+
+    //X-Check if need to send alerts (too soon)
+
+    //X-Send the alerts
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//need to make the count for every function, but not by day
+const getLength = (graphSettings, data) => {
+  // console.log('length', data.length)
+  return data.length
+}
+const getCTRBy = (graphSettings, data) => {
+  const c = data.filter((d) => d.event === 'click').length
+  const p = data.filter((d) => d.event === 'print').length
+
+  //return ratios CTR
+  // console.log('c', c)
+  // console.log('p', p)
+  // console.log('ctr', (c * 100 + 1) / (p + 1))
+  return (c * 100 + 1) / (p + 1)
+}
+const getFunnel = async (graphSettings, data) => {
+  try {
+    let associedTags = new Array(graphSettings.tagUuid.length).fill(undefined)
+    let associedValues = new Array(graphSettings.tagUuid.length).fill(undefined)
+
+    for (const [index, tagUuid] of graphSettings.tagUuid.entries()) {
+      const graph = await Tags.findOne({ where: { uuid: tagUuid } })
+      associedTags[index] = graph.dataValues
+      associedValues[index] = data.filter((d) => d.directiveTag === graph.dataValues.name).length
+    }
+
+    const firstValue = associedValues[0]
+    const last = associedValues[associedValues.length - 1]
+    // console.log(firstValue)
+    // console.log('funnel', (last * 100 + 1) / (firstValue + 1))
+    return (last + 1) / (firstValue + 1)
+  } catch (error) {
+    console.log(error)
   }
 }
